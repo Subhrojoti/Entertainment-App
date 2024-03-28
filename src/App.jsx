d36cebd52d364392d18bbc2e0ad216bb70e6a389
Subhrojoti <subhrojoti20@gmail.com>
2024-03-28 15:55:37 +0530

    conflict modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,12 @@ import MoviesPage from "./components/MoviesPage";
 import SearchBar from "./components/SearchBar";
 import SideBar from "./components/SideBar";
 import TrendingCard from "./components/TrendingCard";
-<<<<<<< HEAD
 import Home from "./components/Home";
 import DetailsPage from "./components/DetailsPage";
-=======
-// import TvSeriesPage from "./components/TvSeriesPage";
->>>>>>> e70db682854643008022bb20cda80fce7bdd1c79
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       {/* <SearchBar />
       <SideBar />
       <h1>Entertainment App</h1> */}
@@ -23,19 +18,7 @@ function App() {
       {/* <TrendingCard /> */}
       {/* </div> */}
       {/* <DetailsPage /> */}
-      <Home />
-=======
-      {/* <SearchBar /> */}
-      {/* <SideBar /> */}
-      {/* <h1>Entertainment App</h1> */}
-      {/* <div className="testing">
-        <Card />
-        <TrendingCard />
-      </div> */}
-
- <MoviesPage />
- {/* <TvSeriesPage /> */}
->>>>>>> e70db682854643008022bb20cda80fce7bdd1c79
+      <Home placeholder="Search for movies" />
     </>
   );
 }
