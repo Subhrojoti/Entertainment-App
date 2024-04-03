80361e1b9789cae3cd6c33a7639d388b2a0eec0b
Subhrojoti <subhrojoti20@gmail.com>
2024-04-03 20:26:10 +0530

    Added Loader added search Functionality and done some bug fixing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,12 @@ import SearchBar from "./SearchBar";
 import SideBar from "./SideBar";
 import TrendingCard from "./TrendingCard";
 import { MdChevronLeft, MdChevronRight, MdLocalMovies } from "react-icons/md";
+import { IoSearch } from "react-icons/io5";
 import axios from "axios";
+import { PiTelevision } from "react-icons/pi";
+import Loader from "./Loader";
 
-const Home = ({ placeholder }) => {
+const Home = () => {
   const slideLeft = () => {
     var slider = document.getElementById("slider");
     slider.scrollLeft = slider.scrollLeft - 500;
@@ -21,6 +24,7 @@ const Home = ({ placeholder }) => {
   const base_url = "https://api.themoviedb.org/3";
   const [moviesData, setMoviesData] = useState([]);
   const [trendingData, setTrendingData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const urlForTrending = `${base_url}/trending/all/day?language=en-US${API_Key}`;
@@ -28,7 +32,8 @@ const Home = ({ placeholder }) => {
     const getTrendingData = async () => {
       try {
         const { data } = await axios.get(urlForTrending);
-        console.log(data);
+        // console.log(data);
+
         // Ensure that data.results exists and is an array before setting state
         if (data && data.results && Array.isArray(data.results)) {
           setTrendingData(data.results);
@@ -42,13 +47,24 @@ const Home = ({ placeholder }) => {
     getTrendingData();
   }, []);
 
+  const [searchQuery, setSearchQuery] = React.useState("");
+
   useEffect(() => {
     const urlRecomendedForYou = `${base_url}/movie/top_rated?language=en-US&page=1${API_Key}`;
+    const urlForSearch = `${base_url}/search/multi?api_key=f69acf74b5c812b81e0ece6ad96116a1`;
 
     const getRecData = async () => {
       try {
-        const { data } = await axios.get(urlRecomendedForYou);
-        console.log(data);
+        const endpoints = searchQuery ? urlForSearch : urlRecomendedForYou;
+
+        const { data } = await axios.get(endpoints, {
+          params: {
+            query: searchQuery,
+          },
+        });
+        setLoading(false);
+
+        // console.log(data);
         // Ensure that data.results exists and is an array before setting state
         if (data && data.results && Array.isArray(data.results)) {
           setMoviesData(data.results);
@@ -60,7 +76,7 @@ const Home = ({ placeholder }) => {
       }
     };
     getRecData();
-  }, []);
+  }, [searchQuery]);
 
   return (
     <div className={home.main}>
@@ -69,71 +85,97 @@ const Home = ({ placeholder }) => {
       </section>
       <section className={home.content}>
         <div className={home.searchBar}>
-          <SearchBar placeholder="Search for movies and TV Series" />
-        </div>
-        <div className={home.trending}>
-          <div className={home.trendingtxt}>
-            <h1>Trending</h1>
-          </div>
-          <div className="relative flex items-center">
-            <MdChevronLeft
-              onClick={slideLeft}
-              size={30}
-              className="cursor-pointer text-white opacity-50 hover:opacity-100"
-            />
-            <div
-              id="slider"
-              className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide"
-            >
-              {trendingData.map((items, i) => {
-                return (
-                  <div
-                    key={i}
-                    className="inline-block cursor-pointer hover:scale-105 ease-in-out duration-200 p-4"
-                  >
-                    <TrendingCard
-                      title={items.title}
-                      poster={items.backdrop_path}
-                      date={items.release_date || items.first_air_date}
-                      name={items.name}
-                      mediaType={items.media_type}
-                      id={items.id}
-                      type={items.type}
-                    />
-                  </div>
-                );
-              })}
-            </div>
-            <MdChevronRight
-              onClick={slideRight}
-              size={30}
-              className="cursor-pointer text-white opacity-50 hover:opacity-100"
+          <div className="input-wrapper">
+            <IoSearch id="search-icon" />
+            <input
+              type="text"
+              placeholder="Search For Movie And Tv Series"
+              onChange={(e) => {
+                setSearchQuery(e.target.value);
+                console.log(e.target.value);
+              }}
             />
           </div>
         </div>
-        <div className={home.recforyou}>
-          <div className={home.recforyoutxt}>
-            <h2>Recommended for you</h2>
+        {loading ? (
+          <div className="w-full flex items-center justify-center ml-28">
+            <Loader />
           </div>
-          <div className={home.recforyouTrendingCard}>
-            {moviesData.map((items, i) => {
-              return (
-                <div className={home.individualRecCard} key={i}>
-                  <Card
-                    title={items.title}
-                    poster={items.backdrop_path}
-                    date={items.release_date}
-                    name={items.name}
-                    logo={<MdLocalMovies />}
-                    mediaType={items.media_type}
-                    id={items.id}
-                    type={items.type}
-                  />
+        ) : (
+          <>
+            <div className={home.trending}>
+              <div className={home.trendingtxt}>
+                <h1>Trending</h1>
+              </div>
+              <div className="relative flex items-center">
+                <MdChevronLeft
+                  onClick={slideLeft}
+                  size={30}
+                  className="cursor-pointer text-white opacity-50 hover:opacity-100"
+                />
+                <div
+                  id="slider"
+                  className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+                >
+                  {trendingData.map((items, i) => {
+                    return (
+                      <div
+                        key={i}
+                        className="inline-block cursor-pointer hover:scale-105 ease-in-out duration-200 p-4"
+                      >
+                        <TrendingCard
+                          title={items.title}
+                          poster={items.backdrop_path}
+                          date={items.release_date || items.first_air_date}
+                          name={items.name}
+                          mediaType={items.media_type}
+                          id={items.id}
+                          type={items.type}
+                        />
+                      </div>
+                    );
+                  })}
                 </div>
-              );
-            })}
-          </div>
-        </div>
+                <MdChevronRight
+                  onClick={slideRight}
+                  size={30}
+                  className="cursor-pointer text-white opacity-50 hover:opacity-100"
+                />
+              </div>
+            </div>
+            <div className={home.recforyou}>
+              <div className={home.recforyoutxt}>
+                <h2>Recommended for you</h2>
+              </div>
+              <div className={home.recforyouTrendingCard}>
+                {moviesData.map((items, i) => {
+                  const logo =
+                    items.media_type === "tv" ? (
+                      <PiTelevision />
+                    ) : (
+                      <MdLocalMovies />
+                    );
+
+                  return (
+                    <div className={home.individualRecCard} key={i}>
+                      <Card
+                        title={items.title}
+                        poster={items.backdrop_path}
+                        date={items.release_date}
+                        releaseData={items.first_air_date}
+                        name={items.name}
+                        logo={logo}
+                        mediaType={items.media_type}
+                        id={items.id}
+                        type={items.type}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </>
+        )}
       </section>
     </div>
   );
--- src/components/MoviesPage.jsx
+++ src/components/MoviesPage.jsx
@@ -6,21 +6,34 @@ import Card from "./Card";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { MdLocalMovies } from "react-icons/md";
+import { IoSearch } from "react-icons/io5";
+import Loader from "./Loader";
 
 const MoviesPage = () => {
   const API_Key = "&api_key=f69acf74b5c812b81e0ece6ad96116a1";
   const base_url = "https://api.themoviedb.org/3";
   const [moviesData, setMoviesData] = useState([]);
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const urlMovies = `${base_url}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc${API_Key}`;
+    const urlForSearch = `${base_url}/search/movie?api_key=f69acf74b5c812b81e0ece6ad96116a1`;
 
     const getMovieData = async () => {
       try {
-        const { data } = await axios.get(urlMovies);
-        console.log(data);
+        const endpoints = searchQuery ? urlForSearch : urlMovies;
+
+        const { data } = await axios.get(endpoints, {
+          params: {
+            query: searchQuery,
+          },
+        });
+        // console.log(data);
         // Ensure that data.results exists and is an array before setting state
         if (data && data.results && Array.isArray(data.results)) {
           setMoviesData(data.results);
+          setLoading(false);
         } else {
           console.error("Unexpected response format:", data);
         }
@@ -29,36 +42,58 @@ const MoviesPage = () => {
       }
     };
     getMovieData();
-  }, []);
+  }, [searchQuery]);
 
   return (
-    <div className="moviesPage">
-      <div className="sideBar">
-        <SideBar />
-      </div>
-      <div className="mainContent">
-        <div className="searchBar">
-          <SearchBar placeholder="Search for movies" />
+    <>
+      <div className="moviesPage">
+        <div className="sideBar">
+          <SideBar />
         </div>
-        <div className="heading">Movies</div>
-        <div className="card-container">
-          {moviesData.map((items, i) => {
-            return (
-              <Card
-                key={i}
-                title={items.title}
-                poster={items.backdrop_path}
-                date={items.release_date}
-                logo={<MdLocalMovies />}
-                id={items.id}
-                type={items.type}
-                imdbId={items.imdb_id} // only this thing added nothing more
+        <div className="mainContent">
+          <div className="searchBar">
+            {/**----------------------------------------------------------------- */}
+            <div className="input-wrapper">
+              <IoSearch id="search-icon" />
+              <input
+                type="text"
+                placeholder="Search For Movies"
+                onChange={(e) => {
+                  setSearchQuery(e.target.value);
+                  // console.log(e.target.value);
+                }}
               />
-            );
-          })}
+            </div>
+            {/**----------------------------------------------------------------- */}
+          </div>
+          {loading ? (
+            <div className="w-full flex items-center justify-center ml-28">
+              <Loader />
+            </div>
+          ) : (
+            <>
+              <div className="heading">Movies</div>
+              <div className="card-container">
+                {moviesData.map((items, i) => {
+                  return (
+                    <Card
+                      key={i}
+                      title={items.title}
+                      poster={items.backdrop_path}
+                      date={items.release_date}
+                      logo={<MdLocalMovies />}
+                      id={items.id}
+                      type={items.type}
+                      imdbId={items.imdb_id} // only this thing added nothing more
+                    />
+                  );
+                })}
+              </div>
+            </>
+          )}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
